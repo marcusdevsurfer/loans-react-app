6afd740bf0ce3b9dcc999aa1e1a802109991ac50
marcusdev99 <marcus.sea13@gmail.com>
2023-12-08 17:18:26 -0600

    feat: addition of delete button

--- src/components/Loan.jsx
+++ src/components/Loan.jsx
@@ -1,12 +1,32 @@
 
-export const Loan = ({loan}) => {
+export const Loan = ({ loan }) => {
 
-  const {loanId, person, quantity} = loan
-  
+  const { loanId, person, quantity } = loan
+
+  const deleteLoan = (e) => {
+
+    fetch('http://localhost:8080/loans/' + e.target.value, {
+      method: 'DELETE',
+      mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer',
+    })
+      .then(res => res.text()) // or res.json()
+      .then(res => window.location.reload())
+      
+  }
+
   return (
-    <div key={loanId}>
+    <div className="mb-3" key={loanId}>
       <h3>{person}</h3>
       <p>{quantity}</p>
+      <button value={loanId} className="btn btn-danger" onClick={deleteLoan}>Borrar</button>
     </div>
   )
 }
