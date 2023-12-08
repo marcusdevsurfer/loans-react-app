531d0eb7b8681cff049862ea00df5090c89b60c2
marcusdev99 <marcus.sea13@gmail.com>
2023-12-08 16:56:13 -0600

    feat: addition of button actions component

---
+++ src/components/Actions.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Actions = () => {
+  return (
+    <div>
+        <div>
+            <button className='btn btn-primary'>Añadir</button>
+        </div>
+    </div>
+  )
+}
