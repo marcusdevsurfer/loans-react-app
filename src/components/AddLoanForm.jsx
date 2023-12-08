8a20db0799a4ea1fa26b977e4ce734b7b9293436
marcusdev99 <marcus.sea13@gmail.com>
2023-12-08 16:55:49 -0600

    feat: addition of form to add loans

---
+++ src/components/AddLoanForm.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react"
+
+export const AddLoanForm = () => {
+
+    const [person, setPerson] = useState('')
+    const [quantity, setQuantity] = useState(0)
+    const [percent, setPercent] = useState(0)
+
+    // Ejemplo implementando el metodo POST:
+    async function postData(url = '', data = {}) {
+        // Opciones por defecto estan marcadas con un *
+        const response = await fetch(url, {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+        });
+        return response.json(); // parses JSON response into native JavaScript objects
+    }
+
+
+
+    const onFormSubmit = (e) => {
+        const loan = {
+            "person": person,
+            "quantity": quantity,
+            "percent": percent
+        }
+        if(loan.person===''){
+            return
+        }
+
+        postData('http://localhost:8080/loans', loan)
+            .then(data => {
+                console.log(data); // JSON data parsed by `data.json()` call
+            });
+
+    }
+
+    return (
+        <form onSubmit={onFormSubmit} className='form w-50'>
+            <div className='mb-3'>
+                <label htmlFor="person">Persona</label>
+                <input value={person} type="text" className='form-control' id='person' onChange={e => setPerson(e.target.value)} />
+            </div>
+
+            <div className='mb-3'>
+                <label htmlFor="quantity">Cantidad</label>
+                <input value={quantity} type="number" className='form-control' id='quantity' onChange={e => setQuantity(e.target.value)} />
+            </div>
+
+            <div className='mb-3'>
+                <label htmlFor="percent">Porciento de Interes</label>
+                <input value={percent} type="number" className='form-control' id='percent' onChange={e => setPercent(e.target.value)} />
+            </div>
+
+            <div className='mb-3'>
+                <button type='submit' className='btn btn-success'>Agregar</button>
+            </div>
+        </form>
+    )
+}
