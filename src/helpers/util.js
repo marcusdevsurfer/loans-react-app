019ca6f6351c7a22a1541221bcde379d09bb8a80
marcusdev99 <marcus.sea13@gmail.com>
2023-12-08 14:42:15 -0600

    refact: addition of request function

---
+++ src/helpers/util.js
@@ -0,0 +1,8 @@
+export const request = async () => {
+    try {
+        const response = await fetch('http://localhost:8080/loans')
+        const data = await response.json()
+        return data
+    }
+    catch {error => console.log(error)}
+}
