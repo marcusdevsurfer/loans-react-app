2c709079baba026f4f3fc7c230ed98d1a5af3724
marcusdev99 <marcus.sea13@gmail.com>
2023-12-10 16:25:33 -0600

    feat: addition of loans summary component

---
+++ src/components/LoansSummary.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const LoansSummary = ({ data }) => {
+
+    const caltLoansTotal = () => {
+        let total = 0
+        data.map((e) => {
+            total = e.quantity+total
+        })
+        return total
+    }
+
+    return (
+        <div className='d-flex justify-content-between'>
+            <div>
+                <p>Activos: <span className='bg-secondary' style={{
+                    paddingLeft: '1rem',
+                    paddingRight: '1rem',
+                    color: 'white'
+                }}>{data.length}</span></p>
+            </div>
+            <div>
+                <p>Total: <span className='bg-secondary' style={{
+                    paddingLeft: '1rem',
+                    paddingRight: '1rem',
+                    color: 'white'
+                }}>{caltLoansTotal()}</span></p>
+            </div>
+        </div>
+    )
+}
