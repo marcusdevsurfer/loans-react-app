424574c0aeb1dce526163bb1ccf88316240bda78
marcusdev99 <marcus.sea13@gmail.com>
2023-12-10 14:28:35 -0600

    feat: addition of function to add loan

--- src/components/AddLoanForm.jsx
+++ src/components/AddLoanForm.jsx
@@ -6,41 +6,39 @@ export const AddLoanForm = () => {
     const [quantity, setQuantity] = useState(0)
     const [percent, setPercent] = useState(0)
 
-    // Ejemplo implementando el metodo POST:
     async function postData(url = '', data = {}) {
-        // Opciones por defecto estan marcadas con un *
         const response = await fetch(url, {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, *cors, same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin', 
             headers: {
                 'Content-Type': 'application/json'
-                // 'Content-Type': 'application/x-www-form-urlencoded',
+      
             },
-            redirect: 'follow', // manual, *follow, error
-            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-            body: JSON.stringify(data) // body data type must match "Content-Type" header
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(data) 
         });
-        return response.json(); // parses JSON response into native JavaScript objects
+        return response.json();
     }
 
 
 
-    const onFormSubmit = (e) => {
-        e.preventDefault()
+     const onFormSubmit =  (e) => {
         const loan = {
             "person": person,
             "quantity": quantity,
             "percent": percent
         }
+        
         if (loan.person === '') {
             return
         }
 
         postData('http://localhost:8080/loans', loan)
             .then(data => {
-                console.log(data); // JSON data parsed by `data.json()` call
+                console.log(data)
             });
 
     }
@@ -64,8 +62,8 @@ export const AddLoanForm = () => {
                     <input value={percent} type="number" className='form-control' id='percent' onChange={e => setPercent(e.target.value)} />
                 </div>
 
-                <div className='mb-3'>
-                    <button type='submit' className='btn btn-success'>Agregar</button>
+                <div className='mb-3 text-center'>
+                    <button type='submit' className='btn btn-sm btn-success'>Agregar</button>
                 </div>
             </form>
 
