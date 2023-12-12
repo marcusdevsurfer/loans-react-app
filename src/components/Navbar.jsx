9394d0aeede5f1caae93605098f648b3026ce777
marcusdev99 <marcus.sea13@gmail.com>
2023-12-12 15:57:57 -0600

    feat: addition of static name of user

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 export const Navbar = () => {
     return (
         <div className='d-flex justify-content-end w-100' style={{
-            backgroundColor: 'black'
+            backgroundColor: 'black',
+            color: 'white'
         }} >
+            <div className="p-2">
+                Welcome, Marcus
+            </div>
         </div>
     )
 }
