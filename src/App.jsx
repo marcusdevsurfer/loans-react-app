281b9358348c431d854219fe0bd68112e569304d
marcusdev99 <marcus.sea13@gmail.com>
2023-12-12 14:53:23 -0600

    refact: deletion of env test

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@ import { AddLoanForm } from "./components/AddLoanForm";
 import { LoansGrid } from "./components/LoansGrid";
 
 function App() {
-  const env = import.meta.env
   return (
     <div className="d-flex flex-column align-items-center">
-      <h1>ENV MODE : {env.VITE_MODE}</h1>
       <Navbar />
       <AddLoanForm />
       <LoansGrid />
