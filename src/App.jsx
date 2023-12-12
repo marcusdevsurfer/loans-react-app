45bb03c79b6c63748be982b7c9e2a1ce797e6881
marcusdev99 <marcus.sea13@gmail.com>
2023-12-12 13:45:24 -0600

    refact: env mode test 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { Actions } from "./components/Actions";
+import { Navbar } from "./components/Navbar";
 import { AddLoanForm } from "./components/AddLoanForm";
 import { LoansGrid } from "./components/LoansGrid";
 
 function App() {
+  const env = import.meta.env
   return (
     <div className="d-flex flex-column align-items-center">
+      <h1>ENV MODE : {env.VITE_MODE}</h1>
       <Navbar />
       <AddLoanForm />
       <LoansGrid />
