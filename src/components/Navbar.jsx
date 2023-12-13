04e74b0ab83d57537818dc8d61ea7f92bb5cc69d
marcusdev99 <marcus.sea13@gmail.com>
2023-12-13 13:23:44 -0600

    feat: addition of log out button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,18 @@
 export const Navbar = () => {
+
+    const handleLogOut = () => {
+        localStorage.removeItem('user')
+    }
+
     return (
         <div className='d-flex justify-content-end w-100' >
             <div className="p-2">
-                Bienvenido, {localStorage.getItem('user')}
+                <p>Bienvenido, {localStorage.getItem('user')}</p>
+            </div>
+            <div className="p-2">
+                <form onSubmit={handleLogOut}>
+                    <button type="submit" className="btn-sm btn btn-danger">Cerrar sesion</button>
+                </form>
             </div>
         </div>
     )
