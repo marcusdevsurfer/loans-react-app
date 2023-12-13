b6f42d89e0f72a4cae027e7046af97812350f765
marcusdev99 <marcus.sea13@gmail.com>
2023-12-13 13:17:15 -0600

    refact: change of static text to local storage value

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ export const Navbar = () => {
     return (
         <div className='d-flex justify-content-end w-100' >
             <div className="p-2">
-                Bienvenido, Marcus
+                Bienvenido, {localStorage.getItem('user')}
             </div>
         </div>
     )
