e348b476d105cfa8b6967c24e9315a8726b7489c
marcusdev99 <marcus.sea13@gmail.com>
2023-12-13 13:16:53 -0600

    refact: addition of states to manage the sessions of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
+import { useEffect, useState } from "react";
+
 import { Navbar } from "./components/Navbar";
 import { AddLoanForm } from "./components/AddLoanForm";
 import { LoansGrid } from "./components/LoansGrid";
+import { Login } from "./components/Login";
 
 function App() {
+
+  const [session, setSession] = useState(localStorage)
+  const [isLogged, setIsLogged] = useState(false)
+
+  useEffect(() => {
+    if(localStorage.length>0){
+      setIsLogged(true)
+    }
+  },[session])
+
   return (
+    isLogged 
+    ?
     <div className="d-flex flex-column align-items-center">
       <Navbar />
       <AddLoanForm />
       <LoansGrid />
     </div>
+    :
+    <Login/>
   )
 }
 
