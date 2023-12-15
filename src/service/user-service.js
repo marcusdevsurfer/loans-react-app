269350c8bf212ba5d9cc6f263d2e167ded7f8b65
marcusdev99 <marcus.sea13@gmail.com>
2023-12-14 18:15:04 -0600

    refact: clean code and addition of fetch user by id in service

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,7 +1,14 @@
+const env = import.meta.env
+
 export const fetchAllUsers = async() => {
-    const env = import.meta.env
-    const URL_FIX = 'users'
+    const URL_FIX = 'api/users'
     const response = await fetch(env.VITE_BASE_URL+URL_FIX)
     const data = await response.json()
     return data
+}
+
+export const fetchUserById = async(id) => {
+    const URL_FIX = 'api/users/'+id
+    const response = await fetch(env.VITE_BASE_URL+URL_FIX)
+    console.log(response)
 }
