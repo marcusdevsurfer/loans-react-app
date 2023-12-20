66b180adcc9b61d6c35b742c867a9f52076c594b
marcusdev99 <marcus.sea13@gmail.com>
2023-12-19 22:34:38 -0600

    refact: change of styles and addition of names and id's for modal

--- src/components/AddLoanForm.jsx
+++ src/components/AddLoanForm.jsx
@@ -10,7 +10,9 @@ export const AddLoanForm = () => {
 
     const user = localStorage.getItem('user')
     const userObj = JSON.parse(user)
-    const {name} = userObj
+    const { name } = userObj
+
+
 
     async function postData(url = '', data = {}) {
         const response = await fetch(url, {
@@ -51,28 +53,41 @@ export const AddLoanForm = () => {
 
     return (
         <>
-            <h2 className="m-3">Agregar</h2>
-            <form onSubmit={onFormSubmit} className='form w-50'>
-                <div className='mb-3'>
-                    <label htmlFor="person">Persona</label>
-                    <input value={person} type="text" className='form-control' id='person' onChange={e => setPerson(e.target.value)} />
-                </div>
+            <button type="button" className="btn btn-sm btn-success" data-bs-toggle="modal" data-bs-target="#addLoanFormModal">
+                Agregar
+            </button>
 
-                <div className='mb-3'>
-                    <label htmlFor="quantity">Cantidad</label>
-                    <input value={quantity} type="number" className='form-control' id='quantity' onChange={e => setQuantity(e.target.value)} />
-                </div>
+            <div className="modal fade" id="addLoanFormModal" tabIndex={-1} aria-labelledby="addLoanFormLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="addLoanFormLabel">Agregar prestamo</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form onSubmit={onFormSubmit} className='form'>
+                                <div className='mb-3'>
+                                    <label htmlFor="person">Persona</label>
+                                    <input value={person} type="text" className='form-control' id='person' onChange={e => setPerson(e.target.value)} />
+                                </div>
 
-                <div className='mb-3'>
-                    <label htmlFor="percent">Porciento de Interes</label>
-                    <input value={percent} type="number" className='form-control' id='percent' onChange={e => setPercent(e.target.value)} />
-                </div>
+                                <div className='mb-3'>
+                                    <label htmlFor="quantity">Cantidad</label>
+                                    <input value={quantity} type="number" className='form-control' id='quantity' onChange={e => setQuantity(e.target.value)} />
+                                </div>
 
-                <div className='mb-3 text-center'>
-                    <button type='submit' className='btn btn-sm btn-success'>Agregar</button>
+                                <div className='mb-3'>
+                                    <label htmlFor="percent">Porciento de Interes</label>
+                                    <input value={percent} type="number" className='form-control' id='percent' onChange={e => setPercent(e.target.value)} />
+                                </div>
+                                <div className='mb-3 text-center'>
+                                    <button type='submit' className='btn btn-sm btn-success'>Agregar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-            </form>
-
+            </div>
         </>
 
     )
