8fdda9ca24a9db9c8a9f29d708d361806d70f16e
marcusdev99 <marcus.sea13@gmail.com>
2023-12-19 23:29:08 -0600

    refact: code refactor

--- src/components/LoansGrid.jsx
+++ src/components/LoansGrid.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { Loan } from "./Loan"
 import { LoansSummary } from "./LoansSummary"
 import { fetchAllLoans } from "../service/loan-service"
+import { AddLoanForm } from "./AddLoanForm"
 
 const LoansTitle = ({ data }) => (
     data.length > 0
@@ -18,7 +19,10 @@ const LoadingLoans = () => (
 )
 
 const NotExistLoansMessage = () => (
-    <h2>No hay registros</h2>
+    <div className="d-flex flex-column align-items-center justify-content-center">
+        <AddLoanForm />
+        <h2>No hay registros</h2>
+    </div>
 )
 
 export const LoansGrid = () => {
@@ -29,6 +33,9 @@ export const LoansGrid = () => {
 
     const user = localStorage.getItem('user')
     const userObj = JSON.parse(user)
+
+    const loansDataFilteredByUser = loansData.filter((loan) => loan.user === userObj.name)
+
 
     useEffect(() => {
 
@@ -48,13 +55,13 @@ export const LoansGrid = () => {
     return (
         isLoaded
             ?
-            loansData.length > 0
+            loansDataFilteredByUser.length > 0
                 ?
                 <div className="m-3">
-                    <LoansTitle data={loansData} />
+                    <LoansTitle data={loansDataFilteredByUser} />
                     <LoansSummary data={loansData} />
                     {
-                        loansData.filter((loan) => loan.user === userObj.name).map((loan) => (
+                        loansDataFilteredByUser.map((loan) => (
                             <Loan key={loan.loanId} loan={loan}></Loan>
                         ))
                     }
