e1055c44457fa318072fce122fb1389e4154fd4e
marcusdev99 <marcus.sea13@gmail.com>
2023-12-20 15:07:37 -0600

    refact: change of env variable

--- src/service/loan-service.js
+++ src/service/loan-service.js
@@ -1,15 +1,15 @@
-const env = import.meta.env
+const ENV = import.meta.env
 
 export const fetchAllLoans = async () => {
     const URL_FIX = 'api/v1/loans'
-    const response = await fetch(env.VITE_BASE_URL + URL_FIX)
+    const response = await fetch(ENV.VITE_BASE_URL + URL_FIX)
     const data = await response.json()
     return data
 }
 
 export const deleteLoan = async (e) => {
     const URL_FIX = 'loans/'
-    const response = await fetch(env.VITE_BASE_URL + URL_FIX + e.target.value, {
+    const response = await fetch(ENV.VITE_BASE_URL + URL_FIX + target.value, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json'
