573112bdd5b99107e853f2459b812c5e90aad9d3
marcusdev99 <marcus.sea13@gmail.com>
2023-12-20 14:54:43 -0600

    refact: refact of code to individuals components

--- src/components/LoansSummary.jsx
+++ src/components/LoansSummary.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { FaEye } from "react-icons/fa";
 import { AddLoanForm } from './AddLoanForm';
+import { LoansTotalAmount } from './LoansTotalAmount';
+import { LoansActiveTotal } from './LoansActiveTotal';
 
 
 export const LoansSummary = ({ data }) => {
@@ -11,48 +12,15 @@ export const LoansSummary = ({ data }) => {
     const userObj = JSON.parse(user)
     const { name } = userObj
 
-    const caltLoansTotal = () => {
-        let total = 0
-        dataFiltered.map((e) => {
-            total = e.quantity + total
-        })
-        const internationalNumberFormat = new Intl.NumberFormat('en-US')
-        const numberFormated = internationalNumberFormat.format(total)
-        const totalFormated = '$'+numberFormated + '.00'
-        return totalFormated
-    }
-
     useEffect(() => {
-        const result = data.filter((loan) => loan.user === name)
-        setDataFiltered(result)
+        const dataByUsername = data.filter((loan) => loan.user === name)
+        setDataFiltered(dataByUsername)
     }, [])
 
     return (
         <div className='mb-3 d-flex flex-column align-items-center justify-content-center'>
-            <div>
-                <p>Activos: <span className='bg-secondary' style={{
-                    paddingLeft: '1rem',
-                    paddingRight: '1rem',
-                    color: 'white'
-                }}>{dataFiltered.length}</span></p>
-            </div>
-            <div className='mb-3 d-flex align-items-center'>
-
-                    <input style={{
-                        border : 'none',
-                    }}  disabled={true} id='totalAmount' type="text" className='form-control p-0 text-center' value={caltLoansTotal()} />
-
-                    <FaEye className='fs-2 m-1' onClick={() => {
-                        const totalAmount = document.getElementById('totalAmount')
-                        const {type} = totalAmount
-
-                        if(type === 'text'){
-                            totalAmount.type='password'
-                        }else if(type === 'password'){
-                            totalAmount.type='text'
-                        }
-                    }} />
-            </div>
+            <LoansActiveTotal dataFiltered={dataFiltered} />
+            <LoansTotalAmount dataFiltered={dataFiltered} />
             <AddLoanForm />
         </div>
     )
