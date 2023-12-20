d232f901d06ac8e833647ead7c5ba0e227d9456a
marcusdev99 <marcus.sea13@gmail.com>
2023-12-19 23:29:43 -0600

    feature: addition of delete loan service

--- src/service/loan-service.js
+++ src/service/loan-service.js
@@ -1,8 +1,19 @@
 const env = import.meta.env
 
-export const fetchAllLoans = async() => {
+export const fetchAllLoans = async () => {
     const URL_FIX = 'api/v1/loans'
-    const response = await fetch(env.VITE_BASE_URL+URL_FIX)
+    const response = await fetch(env.VITE_BASE_URL + URL_FIX)
     const data = await response.json()
     return data
+}
+
+export const deleteLoan = async (e) => {
+    const URL_FIX = 'loans/'
+    const response = await fetch(env.VITE_BASE_URL + URL_FIX + e.target.value, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    response.status === 200 ? window.location.reload() : console.log(response)
 }
