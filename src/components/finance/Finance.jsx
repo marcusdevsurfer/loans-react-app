9ef7d6152b0839857fdda254ed81ae289313a84d
marcusdev99 <marcus.sea13@gmail.com>
2024-01-04 13:27:59 -0600

    feat: addition of finance component

---
+++ src/components/finance/Finance.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Navbar } from '../Navbar'
+
+export const Finance = () => {
+    return (
+        <>
+            <Navbar />
+            <div className='container text-center mt-3'>
+                <div>
+                    <h2>Finanzas</h2>
+                    <p>Estamos trabajando en esta nueva funcion.</p>
+                </div>
+            </div>
+        </>
+    )
+}
