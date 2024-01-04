33d2244d7d24bfe46df6d64d49df55f5f8d3b11e
marcusdev99 <marcus.sea13@gmail.com>
2024-01-04 12:27:27 -0600

    refact: refactor of navbar compont and addition of useNavigate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,52 @@
 import { useEffect, useState } from "react"
+import { Link, Navigate, useNavigate } from "react-router-dom"
+
 
 export const Navbar = () => {
 
     const [user, setUser] = useState({})
+    const navigate = useNavigate()
 
-    const {name, role} = user
+    const { name, role } = user
 
 
     const handleLogOut = () => {
         sessionStorage.clear()
+        navigate("/")
     }
 
     useEffect(() => {
         const actualUser = sessionStorage.getItem('user')
         const userObj = JSON.parse(actualUser)
         setUser(userObj)
-    },[])
+    }, [])
 
     return (
-        <div className='d-flex justify-content-end w-100' >
-            <div className="p-2">
-                <p>Bienvenido, <strong>{name}</strong></p>
+        <nav className='navbar navbar-expand-lg'>
+            <div className="container-fluid">
+                <div className="d-flex m-1">
+
+                    <Link className="btn btn-sm btn-secondary mx-1 d-flex align-items-center" style={{
+                        textDecoration: "none",
+                        color: "white",
+
+                    }} to={"/"}>Prestamos
+                    </Link>
+
+                </div>
+
+                <div className="d-flex m-1">
+                    <div className="px-2">
+                        <p>Bienvenido, <strong>{name}</strong></p>
+                    </div>
+                    <div className="px-2">
+                        <form onSubmit={handleLogOut}>
+                            <button type="submit" className="btn-sm btn btn-danger">Cerrar sesion</button>
+                        </form>
+                    </div>
+                </div>
+
             </div>
-            <div className="p-2">
-                <form onSubmit={handleLogOut}>
-                    <button type="submit" className="btn-sm btn btn-danger">Cerrar sesion</button>
-                </form>
-            </div>
-        </div>
+        </nav>
     )
 }
