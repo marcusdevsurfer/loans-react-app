69cc85b2ad1d158a6c6130335f7fa80414874f9d
marcusdev99 <marcus.sea13@gmail.com>
2024-01-04 12:21:16 -0600

    feature: addition of / path on routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+
+
 import App from './App'
 
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App/>,
+    },
+  ]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <RouterProvider router={router} />
 )
